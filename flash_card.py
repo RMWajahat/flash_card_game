0bba40c2dedd88e6ae096d1b0f838bbfa7c25ab0
RMWajahat <rajawajahat2003@gmail.com>
2023-11-02 07:37:24 +0500

    multi language feature

--- flash_card.py
+++ flash_card.py
@@ -2,46 +2,77 @@ from tkinter import *
 from time import sleep
 import pandas as pd
 import random
+import sys
 
 
-data = pd.read_csv("french_words.csv")
+data = pd.read_csv("testt.csv")
 
-french = data["French"].to_list()
-english = data["English"].to_list()
 
-new_word_french= random.choice(french)
-new_word_english = english[french.index(new_word_french)]
+print("""\nChoose from following language models:
+      1- French
+      2- English
+      3- Chinese
+      4- Japnese
+      5- Urdu
+      6- Arabic\n\n""")
+
+learn_lang = input("Enter language to learn: ").capitalize()
+
+your_lang = input("Enter your native language : ").capitalize()
+
+
+
+
+
+
+try:
+    learn_lang_list = data[learn_lang].to_list()
+    your_language_list = data[your_lang].to_list()
+except Exception as e:
+    print("Language model ",e, " is not available")
+    sys.exit()
+    
+
+new_word_learn_lang= random.choice(learn_lang_list)
+new_word_your_language = your_language_list[learn_lang_list.index(new_word_learn_lang)]
 
 def change_data():
     canvas.itemconfig(card,image = src_back)
-    canvas.itemconfig(language,text = "English",fill ="beige")
-    canvas.itemconfig(word,text = new_word_english,fill ="white")
+    canvas.itemconfig(language,text = your_lang,fill ="beige")
+    canvas.itemconfig(word,text = new_word_your_language,fill ="white")
 
 def time_out_display():
     canvas.after(5000,change_data)
 
 
 def right_button(event):
-    if len(french)==0:
-        canvas.itemconfig(card,image = src_front)
-        canvas.itemconfig(language,text = "Congratulations! you are all caught up",fill ="black")
-        canvas.itemconfig(word,text = "Completed",fill ="grey")
-    global new_word_french,new_word_english
-    french.remove(new_word_french)
-    english.remove(new_word_english)
+    global new_word_learn_lang,new_word_your_language
+    learn_lang_list.remove(new_word_learn_lang)
+    your_language_list.remove(new_word_your_language)
     wrong_button(event)
     
     
 
 def wrong_button(event):
-    global new_word_french,new_word_english
-    new_word_french= random.choice(french)
-    new_word_english = english[french.index(new_word_french)]
+    global new_word_learn_lang,new_word_your_language
+    if len(learn_lang_list)==0:
+        new_word_learn_lang= "Congratulations! you are all caught up."
+        new_word_your_language = "Completed"
+        
+        canvas.itemconfig(card,image = src_front)
+        canvas.itemconfig(language,text =new_word_learn_lang ,font=("monospace",12,"normal"),fill ="black")
+        canvas.itemconfig(word,text = new_word_your_language ,fill ="green")
+        window.after(2500,window.quit)
+        return
     
-    canvas.itemconfig(card,image = src_front)
-    canvas.itemconfig(language,text = "French",fill ="grey")
-    canvas.itemconfig(word,text = new_word_french,fill ="black")
-    time_out_display()
+    else:
+        new_word_learn_lang= random.choice(learn_lang_list)
+        new_word_your_language = your_language_list[learn_lang_list.index(new_word_learn_lang)]
+        
+        canvas.itemconfig(card,image = src_front)
+        canvas.itemconfig(language,text = learn_lang ,fill ="grey")
+        canvas.itemconfig(word,text = new_word_learn_lang, fill ="black")
+        time_out_display()
 
 
 
@@ -49,11 +80,14 @@ def wrong_button(event):
 window = Tk()
 window.title("Flash Card Game")
 window.config(width=820,height=660,bg="#7FDB96")   # padx=20,pady=20
+window.focus_force()
+window.lift()
 src_front = PhotoImage(file="./images/card_front.png")
 src_back = PhotoImage(file="./images/card_back.png")
 cancel = PhotoImage(file="./images/wrong.png")
 okHa = PhotoImage(file="./images/right.png")
 canvas = Canvas(window,width=800,height=660,bg="#7FDB96",highlightthickness=0)
+
 card = canvas.create_image(400,260,image = src_front)
 right = canvas.create_image(580,580,image = okHa)
 canvas.config(cursor="hand2")
@@ -62,13 +96,13 @@ wrong = canvas.create_image(180,580,image = cancel)
 canvas.tag_bind(wrong,'<ButtonPress-1>',wrong_button)
 
 
-language = canvas.create_text(400,100,text="French",fill="grey",font=("monospace",20,"bold"))
-word = canvas.create_text(400,220,text=new_word_french,fill="black",font=("monospace",44,"bold"))
+language = canvas.create_text(400,100,text=learn_lang,fill="grey",font=("monospace",20,"bold"))
+word = canvas.create_text(400,220,text=new_word_learn_lang,fill="black",font=("monospace",44,"bold"))
 
-time_out_display()
 
 canvas.place(x=20,y=10)
 
+time_out_display()
 
 
 
